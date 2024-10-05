d0fea578c5f22cfd9d924dd9152103d170bc3269
codewithmohit783 <rajeshkrkkb84@gmail.com>
2024-10-05 19:20:31 +0000

    container config done

---
+++ 01-basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountID=653456
+let accountEmail="hgdgfcye@.com"
+var accountPass="6446744"
+accountCity="Jaipur"
+
+// accountID=765547 // Cannot be changed if const is used
+
+accountEmail="ghfdghd67343@.com"
+accountPass="646386"
+accountCity="Dhanbad"
+/*
+We should avoid using var because of the issue in block scope and functional scope
+*/
+
+console.log(accountID);
+
+console.table([accountID, accountEmail, accountPass, accountCity])
