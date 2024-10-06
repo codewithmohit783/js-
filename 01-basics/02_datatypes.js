34281673a322375b7b58ee51b1bcfa440fff3e2a
codewithmohit783 <rajeshkrkkb84@gmail.com>
2024-10-06 19:10:00 +0000

    datatypes

---
+++ 01-basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use_strict"; // Treat all js code as newer version
+
+//alert("3+3")  //We are using node.js not browsers
+
+//Code readability should be high
+
+let name="Mohit"
+let age =21
+let isLogged= true 
+
+//number => 2 to the power 53
+//bigint => 
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => 
+//symbol  => unique
+
+
+//object 
+
+console.log(typeof symbol); // undefined
