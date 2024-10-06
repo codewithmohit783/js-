9d9bd0c9b0b4aea842a8484a7d0843064565f7f1
codewithmohit783 <rajeshkrkkb84@gmail.com>
2024-10-06 19:38:39 +0000

    conversion

---
+++ 01-basics/03_conversiontype.js
@@ -0,0 +1,30 @@
+let score ="mohit"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
+
+// "33" => 33
+// "33abc" => nan
+// null >= 0
+// undefined => nan
+//true => 1 , false => 0
+
+let isLoggedIn= "mohit"
+
+let booleanIsLoggedIn= Boolean(isLoggedIn) 
+//console.log(booleanIsLoggedIn);
+
+//In boolean function
+//1 =>true
+//"" => false
+//"mohit" => true 
+
+let pass= 43
+
+let stringnumber= String(pass);
+console.log(stringnumber);
+console.log( typeof stringnumber);
