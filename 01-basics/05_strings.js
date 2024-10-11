a0db5b47f9c3bf3e436be9c6bb0bf461c1d570a6
codewithmohit783 <rajeshkrkkb84@gmail.com>
2024-10-11 18:40:23 +0000

    strings

---
+++ 01-basics/05_strings.js
@@ -0,0 +1,30 @@
+const name="Mohit"
+const repocount=10
+
+//console.log(name+ repocount +"Value")
+
+console.log(`Hello My name is ${name} and my repo count is ${repocount} `);
+
+const gamename = new String('mohiturfk')
+
+//console.log(gamename[0]);
+
+//console.log(gamename.__proto__);
+
+//console.log(gamename.length);
+
+//console.log(gamename.toUpperCase());
+
+//console.log(gamename.charAt(2));
+
+//console.log(gamename.indexOf('t'));
+
+const newString=gamename.substring(0, 4)
+//console.log(newString);
+
+const anotherString=gamename.slice(-6, 4)
+//console.log(anotherString);
+
+const anostring='     mohit    '
+console.log(anostring);
+console.log(anostring.trim());
