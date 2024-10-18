06734c41816bf8418e053864079f1d281b3a75a1
codewithmohit783 <rajeshkrkkb84@gmail.com>
2024-10-18 17:43:38 +0000

    math and num

---
+++ 01-basics/06nums_and_maths.js
@@ -0,0 +1,30 @@
+const score=857
+// console.log(score);
+
+
+const balance=new Number(100);
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(3));
+
+const Othernum=47.4746
+// console.log(Othernum.toPrecision(3));
+
+const hundred=100000
+//console.log(hundred.toLocaleString('en-IN'));
+
+// ++++++++++++++++   MATHS  +++++++++++++++++
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.1));
+// console.log(Math.floor(4.2));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
