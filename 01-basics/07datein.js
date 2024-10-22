83cb6b0e4a0d63fe5c6462c03df7c22abbd153b8
codewithmohit783 <rajeshkrkkb84@gmail.com>
2024-10-22 18:33:34 +0000

    dateandtime

---
+++ 01-basics/07datein.js
@@ -0,0 +1,15 @@
+// Date
+
+let myDate =new Date();
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toJSON());
+// console.log(typeof myDate);
+
+let mycreatedDate= new Date(2023,9,22)
+//console.log(mycreatedDate.toDateString());
+
+let myTimestamp= Date.now()
+
+console.log(Math.floor(Date.now()/1000));
+
