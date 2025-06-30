eb02936828351ae23ec9a7e4f2798e5aff651e40
Mohit Kumar Singh <rajeshkrkkb84@gmail.com>
2025-06-30 07:10:17 +0000

    arrays01

---
+++ 02-basics/01_arrays.js
@@ -0,0 +1,15 @@
+//array 
+
+const myArr= [3,1,4,9,6]
+
+const myHeroes=["Shaktimaan","Krish"]
+
+const myArr2= new Array(3,1,9,5)
+
+console.log(myArr[3]);
+console.log(myArr2[0]);
+console.log(myHeroes[1]);
+
+myArr2.push(4,6)
+console.log(myArr2);
+
