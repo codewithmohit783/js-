296e4cb4142c00874dba52c855b7918e10f1ce69
Mohit Kumar Singh <rajeshkrkkb84@gmail.com>
2025-07-01 21:56:59 +0000

    objects1

---
+++ 02-basics/03_objects.js
@@ -0,0 +1,12 @@
+//singleton
+
+// objects literals
+const JsUsers = {
+    name: "Mohit",
+    age: 21,
+    email: "sgdgggggg@yahoo.com",
+    location: "dhanbad",
+}
+
+console.log(JsUsers.location);
+console.log(JsUsers["location"]);
