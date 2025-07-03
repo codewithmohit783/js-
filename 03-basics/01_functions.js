fc5b4ce796f53f9830280c7a5b6acf3be995883e
Mohit Kumar Singh <rajeshkrkkb84@gmail.com>
2025-07-03 19:57:37 +0000

    funtions

---
+++ 03-basics/01_functions.js
@@ -0,0 +1,29 @@
+// function sayMynamE(){
+//     console.log("M");
+//     console.log("O");
+//     console.log("H");
+//     console.log("I");
+//     console.log("T");
+// }
+
+// sayMynamE()
+
+// function addTwo(number1,number2){
+//     console.log(number1+number2);
+// }
+
+// addTwo(3,5);
+
+// function logInUsername(username){
+//     return `${username} just logged in`
+// }
+
+// console.log(logInUsername("Mohit"));
+
+//(... -> Rest operator as well as spread Operator)
+function calculateCartPrice(...num1){
+    return num1
+}
+
+console.log(calculateCartPrice(100,200,300,65447));
+
